63c78320bd88f84af6f98d40b8b13f47bf52352e
shmakovpn <shmakovpn@yandex.ru>
2019-05-29 05:46:39 +0300

    change verstion to 1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author='shmakovpn',
     author_email='shmakovpn@yandex.ru',
     url='https://github.com/shmakovpn/django-ocr-server',
-    download_url='https://github.com/shmakovpn/django_ocr_server/archive/1.0.zip',
+    download_url='https://github.com/shmakovpn/django_ocr_server/archive/1.1.zip',
     # desctiption='Django OCR Server',
     long_description=long_description,
     entry_points={
