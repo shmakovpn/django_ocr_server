658acf0f8ef6f1d8f3ac7e4d57974be6e4595841
shmakovpn <shmakovpn@yandex.ru>
2019-05-30 10:06:23 +0300

    commented lines about Django in shinx conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 
-import django
-sys.path.insert(0, os.path.abspath('../..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ocr_server.settings'
-django.setup()
+# import django
+# sys.path.insert(0, os.path.abspath('../..'))
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'ocr_server.settings'
+# django.setup()
 
 
 # -- Project information -----------------------------------------------------
