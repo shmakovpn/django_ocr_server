e4f4a05c89cbc2107d4b5f5c4f5664b158fe08d2
shmakovpn <shmakovpn@yandex.ru>
2019-10-11 06:13:43 +0300

    updated version to 1.3

--- django_ocr_server/__init__.py
+++ django_ocr_server/__init__.py
@@ -3,7 +3,7 @@ django_ocr_server/__init__.py OCR Server
 """
 __author__ = 'shmakovpn <shmakovpn@yandex.ru>'
 __date__ = '2019-04-15,2019-10-11'
-__version__ = '1.2'
+__version__ = '1.3'
 
 
 default_app_config = __package__ + '.apps.DjangoOcrServerConfig'
